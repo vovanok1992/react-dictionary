596d83eabe911f144bdc139f14309983b7e92019
Volodymyr Vyshko <volodymyr.vyshko@playtech.com>
2016-11-23 13:48:02 +0200

    Added Promise polifill

--- dicty-frontend/src/main.js
+++ dicty-frontend/src/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'es6-promise/auto'
 import './styles/main.scss'
 import '../index.html';
 import Layout from './components/Layout.js';
