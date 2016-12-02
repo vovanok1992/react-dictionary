02e6bc53b8bbf847a1f638dc40f5f77fff44f265
Volodymyr Vyshko <volodymyr.vyshko@playtech.com>
2016-12-02 18:02:48 +0200

    added word groups folding

--- dicty-frontend/src/components/WordsGroup.js
+++ dicty-frontend/src/components/WordsGroup.js
@@ -4,12 +4,26 @@
 import React from 'react';
 
 export default class WordsGroup extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {closed: false};
+    }
+
+    headerClicked(){
+        console.log("Clicked")
+        this.setState({closed: !this.state.closed});
+    }
+
     render() {
-        const style = "group" + ((this.props.index % 2 == 0) ? " odd" : " even");
+        const style = "group" + ((this.props.index % 2 == 0) ? " odd" : " even") + (this.state.closed ? " closed" :"");
         return (
             <div className={style}>
-                <div className="group-header">{this.props.header}</div>
-                {this.props.wordsElements}
+                <div onClick={this.headerClicked.bind(this)}
+                    className="group-header">{this.props.header}</div>
+                <div className="wordGroupContent">
+                    {this.props.wordsElements}
+                </div>
             </div>
         );
     }
