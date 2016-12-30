5401854bb1f4a93ad16243f3bae89574ec72789f
Vovan <vovanok1992@gmail.com>
2016-12-30 16:11:15 +0200

    added filter for irregular words

--- dicty-frontend/src/components/IrregularVerbs.js
+++ dicty-frontend/src/components/IrregularVerbs.js
@@ -1,42 +1,76 @@
 import React from "react";
-
-import * as WordActions from "../actions/WordsActions";
+import classNames from "classnames";
 
 export default class IrregularVerbs extends React.Component {
 
-    componentWillMount(){
+    constructor() {
+        super();
+        this.state = {
+            text: ""
+        }
+    }
+
+    componentWillMount() {
         this.props.loadIrrVerbs();
     }
 
+    renderTable(verbs) {
+        return (
+            <table className="irrWordsTable table table-striped table-hover table-responsive">
+            <thead>
+            <tr>
+                <th>Infinitive</th>
+                <th>Past Simple</th>
+                <th>Past Participle</th>
+                <th>Перевод</th>
+            </tr>
+            </thead>
+            <tbody>
+            {verbs}
+            </tbody>
+        </table>);
+    }
+
+    generateTableContent() {
+        return this.props.verbs
+            .filter((word) => {
+                return (word.inf + word.ps + word.pp + word.tr).indexOf(this.state.text) != -1
+            })
+            .map((word, id) => {
+                return <tr key={id} onClick={() => this.props.loadDefinition(word.inf)}>
+                    <td>{word.inf}</td>
+                    <td>{word.ps}</td>
+                    <td>{word.pp}</td>
+                    <td>{word.tr}</td>
+                </tr>
+            });
+    }
+
     render() {
-        if(!this.props.verbs){
+        if (!this.props.verbs) {
             return <div>No content</div>
         }
-
-        const verbs = this.props.verbs.map((word, id) => {
-            return <tr key={id} onClick={()=>{this.props.loadDefinition(word.inf)}}>
-                <td>{word.inf}</td>
-                <td>{word.ps}</td>
-                <td>{word.pp}</td>
-                <td>{word.tr}</td>
-            </tr>
+        const verbsDom = this.generateTableContent();
+        const isWordEmpty = this.state.text.length == 0;
+        const newWordIconClassNames = classNames("glyphicon", {
+            "glyphicon-remove-sign removeIcon": !isWordEmpty,
+            "glyphicon-search": isWordEmpty
         });
 
         return (
             <div className="container">
-                <table className="irrWordsTable table table-striped table-hover table-responsive">
-                    <thead>
-                        <tr>
-                            <th>Infinitive</th>
-                            <th>Past Simple</th>
-                            <th>Past Participle</th>
-                            <th>Перевод</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {verbs}
-                    </tbody>
-                </table>
+                <div className="wordSearch">
+                    <input type="text"
+                           value={this.state.text}
+                           onChange={(e) => this.setState({text: e.target.value})}
+                           className="wordInput"
+                           placeholder="Input your word..."/>
+                    <span onClick={() => this.setState({text: ""})}
+                          className={newWordIconClassNames}/>
+                </div>
+
+                {verbsDom.length > 0 && this.renderTable(verbsDom)}
+                {verbsDom.length == 0 && <div>No matches...</div>}
 
             </div>
         );
